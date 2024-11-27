587b53efef974879a2b1fc843d696dc192d1d846
이창헌 <changbill@naver.com>
2024-11-27 23:03:03 +0900

    feat(exception): add MenuMaximumExceedException and OnlyBeverageException

--- src/main/java/christmas/service/UtecoValidator.java
+++ src/main/java/christmas/service/UtecoValidator.java
@@ -1,9 +1,13 @@
 package christmas.service;
 
 import static christmas.constant.UtecoRegex.ORDER_REGEX;
+import static christmas.constant.menu.UtecoMenuType.BEVERAGE;
 
 import christmas.exception.InvalidDateException;
 import christmas.exception.InvalidOrderException;
+import christmas.exception.MenuMaximumExceedException;
+import christmas.exception.OnlyBeverageException;
+import christmas.model.Order;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,8 +26,19 @@ public class UtecoValidator {
         for(String orderString : splitByComma) {
             Matcher matcher = pattern.matcher(orderString);
             if(!matcher.matches()) {
+                System.out.println(new InvalidOrderException().getMessage());
                 throw new InvalidOrderException();
             }
+        }
+    }
+
+    static void validateOrdersException(List<Order> orders) {
+        if(orders.stream().mapToInt(Order::getOrderQuantity).sum() > 20) {
+            throw new MenuMaximumExceedException();
+        }
+
+        if(orders.stream().map(Order::getMenuType).allMatch(utecoMenuType -> utecoMenuType == BEVERAGE)) {
+            throw new OnlyBeverageException();
         }
     }
 }
